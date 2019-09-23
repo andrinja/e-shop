d19d2f2583c8a33b8ff7b65a80190cba300240af
andrinja <andra.zeberga@gmail.com>
2019-09-23 10:40:06 +0200

    adder cartIcon and cartDropdown, implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
-
+// name export
+import CartIcon  from '../cart-icon/cart-icon.component.jsx';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="Logo"/>
@@ -25,17 +27,20 @@ const Header = ({currentUser}) => (
             ) : (
                 <Link className="option" to="/signin">SIGN IN</Link>
             )}
-            
+            <CartIcon/>
         </div>
+        { hidden ? null : <CartDropDown/> }
     </div>
 );
 
 // allows to access the state - root reducer
 // mapStateToProps can be any name
 // we get 'state' object which is the root reducer
-const mapStateToProps = state => ({
+// instead of (state) you can add nested values. Get this user which is destructed from usrr
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
     // name of property the same as passed on Header
-    // state.user FROM root-reducer.js, currentUser from user.reducer.js
-    currentUser: state.user.currentUser
+    // 'state.user' FROM root-reducer.js, 'currentUser' from user.reducer.js
+    currentUser,
+    hidden
 });
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,11 @@
 // base reducer object that represents all of the state of application
 // code that combines all states together
 import { combineReducers } from 'redux';
+
 import userReducer from './user/user-reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
