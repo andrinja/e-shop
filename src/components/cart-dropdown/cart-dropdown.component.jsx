d27dfebbe9bc9392e4b5ab429ea05b821e7e7aa9
andrinja <andra.zeberga@gmail.com>
2019-09-23 15:54:19 +0200

    fix cart item adding issue

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-// get access inside div of cart items
 import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
 import './cart-dropdown.styles.scss';
-import CartItem from '../cart-item//cart-item.component';
 
 const CartDropdown = ({ cartItems }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-            {cartItems.map(cartItem => (
-                <CartItem key={ cartItem.id } item= { CartItem }/>
-            ))}
-        </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
-)
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
-})
+  cartItems
+});
 
 export default connect(mapStateToProps)(CartDropdown);
